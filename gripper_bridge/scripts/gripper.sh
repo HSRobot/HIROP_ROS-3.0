b897bbf577d6792a3b5837b4b4be00c489731e4f
KongDeLiang <13674059697@163.com>
2020-05-30 16:26:36 +0800

    夹爪参数更新

---
+++ gripper_bridge/scripts/gripper.sh
@@ -0,0 +1,4 @@
+rosservice call /UR51/setGripper "gripperName: 'SerialGripper'"
+rosservice call /UR52/setGripper "gripperName: 'SerialGripper'"
+rosservice call /UR51/connectGripper "{}"
+rosservice call /UR52/connectGripper "{}"
