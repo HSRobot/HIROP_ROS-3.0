ed75426844469c499cf8503ec3f4fb97ff1573c8
KongDeLiang <13674059697@163.com>
2020-08-18 14:54:56 +0800

    合并轨迹模块和力控模块

---
+++ force_bridge/src/main.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include "forceService.h"
+
+int main(int argc, char *argv[])
+{
+
+    ros::init(argc, argv, "force_bridge");
+    ros::NodeHandle n;
+    ros::AsyncSpinner as(1);
+    as.start();
+
+    forceService fb(&n);
+    fb.start();
+    ros::MultiThreadedSpinner spinner(1);
+    spinner.spin();
+    return 0;
+}
+
